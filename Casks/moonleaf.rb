538ab7c904dd4c37dde57822ad09604edc7e0255
naomisphere <satwomii@proton.me>
2026-06-25 23:28:47 -0400

    link patch

--- Casks/moonleaf.rb
+++ Casks/moonleaf.rb
@@ -2,10 +2,10 @@ cask "moonleaf" do
   version "4.0.0"
   sha256 "40c2bbdb894dd65d75d0f2b15dc171a0dbf4cc0870d83e1189e677e7c5578093"
 
-  url "https://github.com/naomisphere/moonleaf/releases/download/v#{version}/moonleaf.dmg"
+  url "https://github.com/parkuoa/moonleaf/releases/download/v#{version}/moonleaf.dmg"
   name "moonleaf"
   desc "✨ The Wallpaper Manager for macOS"
-  homepage "https://github.com/naomisphere/moonleaf"
+  homepage "https://github.com/parkuoa/moonleaf"
 
   livecheck do
     url :url
